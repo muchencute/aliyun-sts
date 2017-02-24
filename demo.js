aef2bcb0349ac3227dd25451d712eb598ee16656
nothingmi <nothingmi@muchencute.com>
2017-02-24 11:29:44 +0800

    aliyun-sts-1 工作流演示

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
 const AliyunSTS = require('./index.js');
 
+// 调用接口
 let aliyunSts = new AliyunSTS('Eud6M1yBZq0Ax3vG', 'euoyxkZ1kdsG0CE5nvm7AuqdNF0m1T&');
 aliyunSts.assumeRole('acs:ram::1889630797507648:role/aliyunossdefaultrole', 'test', 3600, function (status, object) {
     console.log(status);
